489af32075f98d756031d6188b6f3485dddaf57a
Denis <presetslr@gmail.com>
2019-10-03 03:22:02 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0)
-  	return false;
-  return true;
+    if (str.length % 2 == 0){
+        let stack = [];
+        let couples = {};
+        bracketsConfig.forEach(el => couples[el[0]] = el[1]);
+        str.split('').forEach((elem, index) => elem === stack[stack.length-1]||0 ? stack.pop() : stack.push(couples[elem]));
+        return stack.length == 0 ? true : false;
+    }
+    return false;
 }
